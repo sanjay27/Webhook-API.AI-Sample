60890ef0a48ca557a0d486bc6362ab35d9487c09
sanjay27 <sanjay@sanjayjadhav.com>
2017-01-15 14:17:09 +0000

    Update index.js
    changed the default text

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ restService.use(bodyParser.urlencoded({
 restService.use(bodyParser.json());
 
 restService.post('/echo', function(req, res) {
-    var speech = req.body.result && req.body.result.parameters && req.body.result.parameters.echoText ? req.body.result.parameters.echoText : "Seems like some problem. Speak again."
+    var speech = req.body.result && req.body.result.parameters && req.body.result.parameters.echoText ? req.body.result.parameters.echoText : "Sorry I didn't follow you, please Speak again."
     return res.json({
         speech: speech,
         displayText: speech,
